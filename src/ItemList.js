bbfdf1bf276c1b1cf7f66c46de7e027df6d78755
Jouni Kiviperä <jounikivipera@gmail.com>
2022-01-03 13:45:37 +0200

    muutoksia

---
+++ src/ItemList.js
@@ -0,0 +1,16 @@
+import LineItem from './LineItem';
+const ItemList = ({ items, handleCheck, handleDelete }) => {
+    return (
+        <ul>
+            {items.map((item) => (
+                <LineItem item={item} 
+                key={item.id}
+                handleCheck={handleCheck} 
+                handleDelete={handleDelete} 
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default ItemList
